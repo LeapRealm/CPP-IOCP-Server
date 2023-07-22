f2220384f78aed695f607e5b5d4472f49c78213b
JinwooKim <leaprealm@gmail.com>
2023-07-22 22:07:51 +0900

    Day 6

---
+++ GameServer/10_Cache.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "CorePch.h"
+#include <iostream>
+#include <windows.h>
+
+int32 buffer[10000][10000];
+
+void lesson_10()
+{
+	memset(buffer, 0, sizeof(buffer));
+
+	{
+		uint64 start = GetTickCount64();
+
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+		{
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[i][j];
+			}
+		}
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
+
+	{
+		uint64 start = GetTickCount64();
+
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+		{
+			for (int32 j = 0; j < 10000; j++)
+			{
+				sum += buffer[j][i];
+			}
+		}
+
+		uint64 end = GetTickCount64();
+		cout << "Elapsed Tick " << (end - start) << endl;
+	}
+}
